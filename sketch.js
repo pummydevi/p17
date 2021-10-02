6c0e85b9afdbe6d94638ca71b123e8b3b0880587
pummydevi <89304803+pummydevi@users.noreply.github.com>
2021-10-02 18:21:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,150 +1,124 @@
-//Game States
-var PLAY=1;
-var END=0;
-var gameState=1;
-
-var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
-var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
+var bow , arrow,  background;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
 function preload(){
   
-  knifeImage = loadImage("knife.png");
-  monsterImage = loadAnimation("alien1.png","alien2.png")
-  fruit1 = loadImage("fruit1.png");
-  fruit2 = loadImage("fruit2.png");
-  fruit3 = loadImage("fruit3.png");
-  fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("gameover.png")
-
-  //load sound here
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  
 }
 
 
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
   
-  //creating sword
-   knife=createSprite(40,200,20,20);
-   knife.addImage(knifeImage);
-   knife.scale=0.7
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
   
-  //set collider for sword
-  knife.setCollider("rectangle",0,0,40,40);
-
-  // Score variables and Groups
-  score=0;
-  fruitGroup=createGroup();
-  monsterGroup=createGroup();
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
   
 }
 
 function draw() {
-  background("lightblue");
-  
-  if(gameState===PLAY){
-    
-    //Call fruits and Monster function
-    fruits();
-    Monster();
-    
-    // Move sword with mouse
-    knife.y=World.mouseY;
-    knife.x=World.mouseX;
-  
-    // Increase score if sword touching fruit
-    if(fruitGroup.isTouching(knife)){
-      score=score+2;
-      fruitGroup.destroyEach();
+ background(0);
+  // moving ground
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
     }
-    else
-    {
-      // Go to end state if sword touching enemy
-      if(monsterGroup.isTouching(knife)){
-        gameState=END;
-        
-        //add gameover sound here
-        
-        if(fruitGroup.isTouching(knife)){
-          fruitGroup.destroyEach();
-          knifeSwooshSound.play();
-        }
-      
-        
-        monsterGroup.destroyEach();
-        fruitGroup.setVelocityXEach(0);
-        monsterGroup.setVelocityXEach(0);
-        
-        // Change the animation of sword to gameover and reset its position
-        knife.addImage(gameOverImage);
-        knife.scale=2;
-        knife.x=300;
-        knife.y=300;
-      }
+  
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+  
+  //creating continous balloons
+  var select_balloon = Math.round(random(1,1));
+  
+  if (World.frameCount % 100 == 0) {
+    if (select_balloon == 1) {
+      redBalloon();
     }
   }
   
+var select_balloon=Math.round(random(1,4));
+
+if(World.frameCount%100==0){
+  if(select_balloon==1){
+    redBalloon();
+  }  else if(select_balloon==2){
+    greenBalloon();
+  } else if(select_balloon==3){
+    blueBalloon();
+  }else{
+    pinkBalloon();
+  }
+}
+
+
+
+
+
+
+
   drawSprites();
-  //Display score
-  textSize(25);
-  text("Score : "+ score,250,50);
 }
 
 
-function Monster(){
-  if(World.frameCount%200===0){
-    monster=createSprite(400,200,20,20);
-    monster.addAnimation("moving", monsterImage);
-    monster.y=Math.round(random(100,550));
-    //update below give line of code for increase monsterGroup speed by 10
-    monster.velocityX = -8;
-    monster.setLifetime=50;
-    monster.velocityX=-(8+(score/10));
-    monsterGroup.add(monster);
-  }
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
 }
 
-function fruits(){
-  if(World.frameCount%80===0){
-    position = Math.round(random(1,2));
-    fruit=createSprite(400,200,20,20);
-    
-     //using random variable change the position of fruit, to make it more challenging
-    
-    if(position==1)
-    {
-    fruit.x=600;
-    //update below give line of code for increase fruitGroup speed by 4
-    fruit.velocityX=-7
-    }
-    else
-    {
-      if(position==2){
-      fruit.x=0;
-      
-     //update below give line of code for increase fruitGroup speed by 4
-      fruit.velocityX= 7;
-      }
-    }
-    
-    fruit.scale=0.2;
-     //fruit.debug=true;
-     r=Math.round(random(1,4));
-    if (r == 1) {
-      fruit.addImage(fruit1);
-    } else if (r == 2) {
-      fruit.addImage(fruit2);
-    } else if (r == 3) {
-      fruit.addImage(fruit3);
-    } else {
-      fruit.addImage(fruit4);
-    }
-    
-    fruit.y=Math.round(random(50,550));
-   
-    
-    fruit.setLifetime=100;
-    
-    fruitGroup.add(fruit);
-  }
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+
+}
+
+function blueBalloon() {
+var blue=createSprite(0,Math.round(random(20,370)),10,10);
+blue.addImage(blue_balloon0.png);
+blue.velocityX=4;
+blue.lifetime=150;
+blue.scale=0.1;
+}
+
+function greenBalloon() {
+  var green=createSprite(0,Math.round(random(20,370)),10,10);
+  green.addImage(green_balloon0.png);
+  green.velocityX=3;
+  green.lifetime=150;
+  green.scale=0.1;
+}
+
+function pinkBalloon() {
+  var pink=createSprite(0,Math.round(random(20,370)),10,10);
+  pink.addImage(pink_balloon0.png);
+  pink.velocityX=3;
+  pink.lifetime=150;
+  pink.scale=0.1;
 }
